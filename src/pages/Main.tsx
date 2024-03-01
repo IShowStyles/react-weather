7eb6f82717df1e2af7fa3527ce70412f6783b06b
zholudev111 <zholudev111@gmail.com>
2024-03-01 14:47:25 +0200

    build fixed2

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -92,7 +92,7 @@ const MainPage = () => {
                         </div>
                       </div>
                     </div>
-                    {trips.length >= 2 ? (
+                    {trips.length >= 1 ? (
                       <div className='sliders-btns'>
                         <CommonButton
                           onClick={() => {
